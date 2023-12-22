977f656492184bb7a363759546629edb65080099
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2023-12-22 20:35:06 +0200

    chore: add first step in lexer of project

---
+++ src/Models/lexer.ts
@@ -0,0 +1,36 @@
+
+export enum mainLangBody {
+  Number,
+  Identifier,
+  String,
+  // Variables
+  Ter,
+  See,
+  Fn,
+  If,
+  Else,
+  For,
+  // Operators
+  Binary_Operator,
+  Equals, // =
+  Comma, // ,
+  Colon, // :
+  Semicolon, // ;
+  Dot, // .
+  Open_Paren, // (
+  Close_Paren, // )
+  Open_Brace, // {
+  Close_Brace, // }
+  Open_Bracket, // [
+  Close_Bracket, // ]
+  Quotation, // "
+  Greater, // >
+  Lesser, // <
+  Equals_Compare, // ====
+  Not_Equals_Compare, // !===
+  Exclamation, // !
+  And, // &&
+  Ampersand, // &
+  Bar, // |
+  EOF, // Signified the end of file.
+}
